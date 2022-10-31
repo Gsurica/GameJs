03a078de6a465c1b9a5c3cabf79b151c6a874f83
gsurica <eosurica@gmail.com>
2022-10-31 10:01:26 -0300

    gameJs

---
+++ index.js
@@ -0,0 +1,271 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext('2d')
+
+const score = document.querySelector('#scoreEl')
+const startGamebtn = document.querySelector("#startGameBtn")
+const startGameModal = document.querySelector("#modalEl")
+const bigScore = document.querySelector("#bigScoreEl")
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x 
+    this.y = y 
+    this.radius = radius 
+    this.color = color
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x =  x
+    this.y = y 
+    this.radius = radius 
+    this.color = color 
+    this.velocity = velocity
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x =  x
+    this.y = y 
+    this.radius = radius 
+    this.color = color 
+    this.velocity = velocity
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+const friction = 0.97
+
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x =  x
+    this.y = y 
+    this.radius = radius 
+    this.color = color 
+    this.velocity = velocity
+    this.alpha = 1
+  }
+
+  draw() {
+    ctx.save()
+    ctx.globalAlpha = this.alpha
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.restore()
+  }
+
+  update() {
+    this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
+  }
+}
+
+
+const x = canvas.width / 2
+const y =  canvas.height / 2
+
+let player =  new Player(x, y, 10, 'white')
+
+let projectiles = []
+let enemies = []
+let particles = []
+
+function init() {
+  player =  new Player(x, y, 10, 'white')
+
+  projectiles = []
+  enemies = []
+  particles = []
+  scoreEl = 0
+}
+
+function spawnEnemies () {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
+    let x
+    let y 
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height  
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+
+    const velocity = {
+      x: Math.cos(angle) ,
+      y: Math.sin(angle)
+    }
+
+    enemies.push(new Enemy(
+      x, 
+      y,
+      radius,
+      color,
+      velocity
+    ))
+  }, 1000)
+}
+
+let animationId 
+let scoreEl = 0
+
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  player.draw()
+
+  particles.forEach((particle, index) => {
+
+    if (particles.alpha <= 0) {
+      particles.splice(index, 1)
+    } else {
+      particle.update()
+    }
+
+  })
+  
+  projectiles.forEach((projectile, projectileIndex) => {
+    projectile.update()
+
+    // remove from edges of screen
+    if (
+      projectile.x + projectile.radius < 0 || 
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+      ) {
+      setTimeout(() => {
+        projectiles.splice(projectileIndex, 1)
+      }, 0)
+    }
+  })
+
+  enemies.forEach((enemy, enemyIndex) => {
+    enemy.update()
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId)
+      startGameModal.style.display = 'flex'
+      bigScore.innerHTML = scoreEl
+    }
+
+    projectiles.forEach((projectile, projectileIndex) => {
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+      if (dist - enemy.radius - projectile.radius < 1) {
+
+        // increase score
+
+        for(let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(new Particle(
+            projectile.x,
+            projectile.y,
+            Math.random() * 2,
+            enemy.color,
+            {
+              x: (Math.random() - 0.5) * (Math.random() * 4),
+              y: (Math.random () - 0.5) * (Math.random() * 4)
+            }
+          ))
+        }
+
+        if (enemy.radius - 10 > 5) {
+          scoreEl += 100
+          score.innerHTML = scoreEl
+          gsap.to(enemy, {
+            radius: enemy.radius - 10
+          })
+          setTimeout(() => {
+            projectiles.splice(projectileIndex, 1)
+          }, 0)
+        } else {
+          scoreEl += 250
+          score.innerHTML = scoreEl
+          setTimeout(() => {
+            enemies.splice(enemyIndex, 1)
+            projectiles.splice(projectileIndex, 1)
+          }, 0)
+        }
+
+      }
+    })
+  })
+}
+
+addEventListener('click', e => {
+
+  const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2)
+
+  const velocity = {
+    x: Math.cos(angle) * 4,
+    y: Math.sin(angle) * 4
+  }
+
+  projectiles.push(new Projectile(
+    canvas.width / 2,
+    canvas.height / 2,
+    5,
+    'white',
+    velocity
+  ))
+})
+
+startGamebtn.addEventListener('click', () => {
+  init()
+  animate()
+  spawnEnemies()
+  startGameModal.style.display = 'none'
+})
+
